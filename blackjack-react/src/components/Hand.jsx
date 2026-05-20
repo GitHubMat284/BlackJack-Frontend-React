b4c9e67ddb091fea665630e7a02337705fd7c4c1
Matheo Villacres <mathius85203@gmail.com>
2026-05-19 19:47:31 -0500

    fix: improve elements layout

--- blackjack-react/src/components/Hand.jsx
+++ blackjack-react/src/components/Hand.jsx
@@ -1,18 +1,24 @@
 import Card from "./Card";
+import BackCard from "./BackCard";
 
 export default function Hand({ title, cards = [], total }) {
-  const specialTotal = total > 21 ? "text-red-900" : total === 21 ? "shimmer" : "text-yellow-300";
+  const isDealer = title?.toLowerCase().includes("dealer");
+  const showHiddenCard = isDealer && cards.length === 1;
+  const specialTotal = total > 21 ? "text-red-500" : total === 21 ? "shimmer" : "text-yellow-300";
+
   return (
-    <div className=" p-4 rounded-lg w-full">
-      <h2 className="text-2xl font-bold mb-3 flex justify-between">
+    <div className="p-4 rounded-lg w-full">
+      <h2 className="text-2xl font-bold mb-3 flex justify-center items-center gap-5">
         <span className="text-white">{title}</span>
         <span className={specialTotal}>{total}</span>
       </h2>
 
-      <div className="flex gap-2 flex-wrap">
+      <div className="flex justify-center gap-2 flex-wrap">
         {cards.map((c, i) => (
           <Card key={i} card={c} />
         ))}
+
+        {showHiddenCard && <BackCard />}
       </div>
     </div>
   );
